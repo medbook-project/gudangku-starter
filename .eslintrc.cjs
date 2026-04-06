2029f9a83c906ddb9ebebbede7910401697b2a36
Micheal Seng <9906104+michealseng@users.noreply.github.com>
2026-04-06 16:10:56 +0700

    chore: add project scaffold and configuration files

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'public/mockServiceWorker.js'],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+  },
+  settings: {
+    react: { version: 'detect' },
+  },
+};
